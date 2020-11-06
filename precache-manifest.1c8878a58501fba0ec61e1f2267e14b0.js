4e832c01c93ce4a28f4d888140a6a4e812fb4446
nryusuke <nyusuke@ideapad-l340.lan>
2020-11-06 22:02:42 +0800

    Updates

---
+++ precache-manifest.1c8878a58501fba0ec61e1f2267e14b0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cc645d5639ce6d93f438468bd3ba50",
+    "url": "/pwebapp/index.html"
+  },
+  {
+    "revision": "ac638f243e00d3143338",
+    "url": "/pwebapp/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "f65cda1705ef040cb928",
+    "url": "/pwebapp/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "22a41f4f47634046f38b",
+    "url": "/pwebapp/static/css/main.48db2c47.chunk.css"
+  },
+  {
+    "revision": "6da75c1bdd08166b170e",
+    "url": "/pwebapp/static/js/2.bbd5e602.chunk.js"
+  },
+  {
+    "revision": "ac638f243e00d3143338",
+    "url": "/pwebapp/static/js/3.3f7d2c05.chunk.js"
+  },
+  {
+    "revision": "f65cda1705ef040cb928",
+    "url": "/pwebapp/static/js/4.bfe11bd1.chunk.js"
+  },
+  {
+    "revision": "22a41f4f47634046f38b",
+    "url": "/pwebapp/static/js/main.17c8a6e7.chunk.js"
+  },
+  {
+    "revision": "bd73da4adca02a03775d",
+    "url": "/pwebapp/static/js/runtime-main.c127f632.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/pwebapp/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/pwebapp/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/pwebapp/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "d622c007115172611e9fa454d1658a90",
+    "url": "/pwebapp/static/media/clockGear.d622c007.svg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/pwebapp/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/pwebapp/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/pwebapp/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pwebapp/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/pwebapp/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/pwebapp/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/pwebapp/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/pwebapp/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/pwebapp/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
